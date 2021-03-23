41a7fcaf7290fdaa193dbc909c960ca1d9321779
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-03-23 19:03:06 -0300

    Membros de uma classe parte 2 - Private set, onde é uma funcao que atribui valor a propriedade

--- KOTLIN/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ KOTLIN/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,18 +3,15 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String =  "Nelson"
     var cpf: String =   "999.999.999-99"
+    private set
 
-    inner class Endereco {
-        var rua: String = "Rua Teste"
-    }
 }
 
 fun main() {
     val nelson = Pessoa()
 
+    println(nelson)
     println(nelson.nome)
     println(nelson.cpf)
-
-    println(nelson.Endereco().rua)
 
 }
