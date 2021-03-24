2ea2e8d55e94c70f158afedd78bd5e4d12315427
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-03-24 12:37:11 -0300

    update polimorfism

--- KOTLIN/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ KOTLIN/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -5,8 +5,8 @@ import one.digitalinnovation.digionebank.Gerente
 fun main() {
 
     val maria = Gerente("Maria do Carmo",
-        "123.456.789-11",5000.0)
+        "123.456.789-11",5000.0,"senha1235")
 
     ImprimeRelatorioFuncionario.imprime(maria)
-
+    TesteAutenticacao().autentica(maria)
 }
