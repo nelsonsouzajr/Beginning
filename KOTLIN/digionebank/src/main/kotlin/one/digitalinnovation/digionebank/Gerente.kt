c574e6c83438510fa10e8921d018a013aa35b5d6
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-03-24 00:35:58 -0300

    update Class Person, subclasses Analist, Employees and Mananger

---
+++ KOTLIN/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank
+
+class Gerente(nome: String,
+              cpf: String,
+              salario: Double
+) : Funcionario(nome, cpf, salario,) {
+    override fun calculoAuxilio() = salario * 0.4
+    }
