a8c8b2bd6e184aae29e84d655f1ea12295d3c4ac
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-03-29 21:18:23 -0300

    created int array test class

---
+++ KOTLIN/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteIntArray.kt
@@ -0,0 +1,30 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val values = IntArray(5)
+    values[0]=5
+    values[1]=2
+    values[2]=4
+    values[3]=7
+    values[4]=1
+
+    for(valor in values) {
+        println(valor)
+    }
+
+    println("----------") // proximo comando it toma o nome da variavel, que pode ser alterado por exemplo "valor ->
+    values.forEach {
+        println(it)
+    }
+
+    println("----------")//index é a nossa variavel
+
+    for (index in values.indices){
+        println(values[index])
+    }
+    println("----------")//.sort ordena os valores por padrao em sequencia do menor para o maior
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+}
