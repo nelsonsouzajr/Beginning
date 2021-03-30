208ca22b344007b6a27ae0233e639399862ebc2c
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-03-30 05:02:24 -0300

    groupBy function test class

--- KOTLIN/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ KOTLIN/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val joao = funcionario("Joao",2400.0)
-    val maria = funcionario("Maria",1600.0)
-    val jose = funcionario("José",5000.0)
+    val joao = funcionario("Joao",2400.0, "CLT")
+    val maria = funcionario("Maria",1600.0,"PJ")
+    val jose = funcionario("José",5000.0,"CLT")
 
     val funcionarios = listOf(joao, jose, maria)
     funcionarios.forEach { println(it) }
@@ -11,21 +11,27 @@ fun main() {
     println("----------")
     println(funcionarios.find { it.nome == "José" })
 
-    println("---------")
+    println("---------")//organiza os funcionarios em ordem crescente do salario
     funcionarios
             .sortedBy { it.salario}
             .forEach{ println(it) }
 
+    println("---------")//agrupa os funcionarios em pelo grupo de contratacao
+    funcionarios
+            .groupBy { it.tipo}
+            .forEach{ println(it) }
 
 }
 
 data class funcionario(
         val nome: String,
-        val salario: Double
+        val salario: Double,
+        val tipo: String
 ) {
     override fun toString(): String =
             """
                 Nome:    $nome
                 Salário: $salario
+                
             """.trimIndent()
 }
